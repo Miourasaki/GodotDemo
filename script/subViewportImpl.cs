b1efff21981a37bb58e09ae2ff97290426dd58b3
白澪 · 卡丝蕾特 <mio@chyan.moe>
2024-08-22 22:42:09 +0800

    i

--- script/subViewportImpl.cs
+++ script/subViewportImpl.cs
@@ -10,6 +10,7 @@ public partial class subViewportImpl : SubViewport
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		this.RenderTargetUpdateMode = UpdateMode.Once;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
